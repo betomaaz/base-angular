f1b2c0d232a6e9b1354802873a86bcd65268bab5
ALBERTO JOSE MARRERO AZUAJE <ALBERTO.MARRERO@correoaiep.cl>
2022-04-23 09:24:44 -0400

    Carga proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import {RouterModule} from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  posts : any[] = [1,1,1,1,1];
+
+  constructor(private postService : PostsService) { }
+
+  ngOnInit(): void {
+    this.postService.getPostData()
+    .subscribe((resp:any) =>{
+      console.log(resp);
+      this.posts = resp;
+    })
+  }
+
+}
